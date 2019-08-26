345666b04f3513a2fbb4b593674d46651282dd79
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-26 17:32:49 +0530

    Change json file into a object which as keys

--- Zen3.js
+++ Zen3.js
@@ -1,13 +1,27 @@
 const fs = require("fs");
 fs.readFile("./datafile.json", "utf8", (err, jsonString) => {
   if (err) {
-    console.log("Error reading file from disk:", err);
+    console.log("File read failed:", err);
     return;
   }
-  try {
-    const data_Keys = JSON.parse(jsonString);
-    console.log("keys:", data_Keys.keys);
-  } catch (err) {
-    console.log("Error parsing JSON string:", err);
-  }
+  const data = JSON.parse(jsonString);
+  const result = getTerminal(data);
+  console.log(result);
 });
+
+function getTerminal(data) {
+  const result = Object.keys(data).map(x => {
+    let tempTerminal = data[x];
+    let prevTerminal = tempTerminal;
+    while (!tempTerminal) {
+      prevTerminal = tempTerminal;
+      tempTerminal = data[tempTerminal];
+      if (!tempTerminal) {
+        break;
+      }
+    }
+
+    return { [x]: prevTerminal };
+  });
+  return result;
+}
