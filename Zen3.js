f0b070709f5bad2e3eaca89ab56694d30c09600a
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-26 16:41:37 +0530

    Find keys value in json file

--- Zen3.js
+++ Zen3.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
 fs.readFile("./datafile.json", "utf8", (err, jsonString) => {
   if (err) {
-    console.log("File read failed:", err);
+    console.log("Error reading file from disk:", err);
     return;
   }
-  console.log("File data:", jsonString);
+  try {
+    const data_Keys = JSON.parse(jsonString);
+    console.log("keys:", data_Keys.keys);
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
 });
